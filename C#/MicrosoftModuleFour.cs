d7d75ddb1faeab1179a01ff251661ba6cdecd563
McKinnely <mckinnely.bentley@gmail.com>
2016-10-08 18:52:42 -0500

    Added Microsoft Module Four Instructions Added

---
+++ C#/MicrosoftModuleFour.cs
@@ -0,0 +1,25 @@
+/*
+Prior to object oriented considerations and class files, programmers created structs in languages such as C.  Some programmers still use structs for storing related information, although the trend is more towards class files.  Because there may still be occasions where a struct makes sense in your code, you're going to create some structs in this assignment.  Because a struct has a similar layout to a class file, this will provide you with a layout for the variables in your student, teacher, program, and course aspects.
+
+For this assignment, complete the following tasks.  For the structs, just include member variables and a constructor.  Do not create properties at this time.  Include all the variables that you have created up to this point in time.
+
+Create a struct to represent a student
+Create a struct to represent a teacher
+Create a struct to represent a program
+Create a struct to represent a course
+Create an array to hold 5 student structs.
+Assign values to the fields in at least one of the student structs in the array
+Using a series of Console.WriteLine() statements, output the values for the student struct that you assigned in the previous step
+When complete, submit your code in the peer review section.
+
+Challenge (Do not submit the challenge code to the peer assessment area)
+
+For this challenge, expand on the struct array steps to complete the following:
+
+Use an appropriate looping structure to add values to all student structs in the array by prompting a user of the application to enter values for fields.
+
+For example, if you created fristName, lastName, address, city fields as an example, for each of the 5 students in the array, you need to prompt the user for each field in the struct, for each student struct in the array.
+Once completed, create another loop to iterate over the array and write the values to the console window.
+*/
+
+
