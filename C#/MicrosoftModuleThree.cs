e5be3405cce79c71415cb81c957f7e87b7df82be
McKinnely <mckinnely.bentley@gmail.com>
2016-10-08 18:43:21 -0500

    Added Microsoft Module three Code

--- C#/MicrosoftModuleThree.cs
+++ C#/MicrosoftModuleThree.cs
@@ -43,3 +43,82 @@ For this task, use MSDN to research the NotImplementedException exception.
 Create a new method for validating a student's birthday.  You won't write any validation code in this method, but you will throw the NotImplementedException in this method
 Call the method from Main() to verify your exception is thrown
 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleThreeAssignment
+{
+class ModuleThreeAssignment
+{
+// 5 Call the Get methods from the Main method in the application
+static void Main(string[] args)
+{
+GetStudentInfo();
+GetTermInfo();
+Console.ReadKey(); //Pauses the program 
+
+
+
+}
+
+// 1. Using the below partial code sample, complete the method for getting student data.
+
+static void GetStudentInfo()
+{
+Console.WriteLine("Enter the student's first name: ");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Enter the student's last name");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Enter the student's birth date: ");
+DateTime bDate = DateTime.Parse(Console.ReadLine());
+
+// 6 Call the Print methods from within each Get method.
+PrintStudentDetails(firstName, lastName, bDate);
+
+
+
+// 1. Using the above partial code sample, complete the method for getting student data.
+
+
+}
+
+// 2. Create a method to get information for a teacher, a course, and program, and a degree using a similar method as above.
+// 4. Just enter enough information to show you understand how to use methods. (At least three attributes each).
+static void GetTermInfo()
+{
+Console.WriteLine("Enter the Teacher's name: ");
+string teacherName = Console.ReadLine();
+
+Console.WriteLine("Enter the course name: ");
+string course = Console.ReadLine();
+
+Console.WriteLine("Enter the program name: ");
+string program = Console.ReadLine();
+
+Console.WriteLine("Enter the name of the degree: ");
+string degree = Console.ReadLine();
+
+// 6 Call the Print methods from within each Get method
+PrintTermDetails(teacherName, course, program, degree); 
+}
+
+// 3. Create methods to print the information to the screen for each object such as static void PrintStudentDetails(...)
+
+static void PrintStudentDetails(string first, string last, DateTime birthday)
+{
+Console.WriteLine("{0} {1} was born on: {2}", first, last, birthday.ToShortDateString());
+}
+
+static void PrintTermDetails(string teacherName, string course, string program, string degree)
+{
+Console.WriteLine("Teacher: {0}\n" + "Course: {1}\n" + "Program: {2}\n" + "Degree: {3}\n", teacherName, course, program, degree);
+}
+
+}
+}
