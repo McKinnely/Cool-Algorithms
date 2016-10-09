bdd874973b3e4c63b7a3056b8bf6edb4e9c09e54
McKinnely <mckinnely.bentley@gmail.com>
2016-10-09 13:02:01 -0500

    Finished FCC Longest Word Algorithm

---
+++ JavaScript/LongestWord.js
@@ -0,0 +1,16 @@
+function findLongestWord(str) 
+    {
+    str = str.split(" ");
+    var size = str.length;
+    var counter = 0;
+    var arrayCopy = [];
+    while(size > counter )
+    { 
+    arrayCopy[counter] = str[counter].length;
+    counter++;
+    }
+    var max = Math.max.apply(Math, arrayCopy);
+    var indexTopValueLocator = arrayCopy.indexOf(max);
+    return str[indexTopValueLocator].length;
+   }
+findLongestWord("Energy can neither be created or destroyed!");
