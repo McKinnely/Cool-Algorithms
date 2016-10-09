a2408d562abde76f49981dd018823ad2419ab141
McKinnely <mckinnely.bentley@gmail.com>
2016-10-08 19:09:05 -0500

    Added Microsoft Module Ten Instructions Added

---
+++ C#/MicrosoftModuleTen.cs
@@ -0,0 +1,26 @@
+/*
+For the first part of this assignment, complete the following:
+
+Open the LINQ Restriction Operators project file
+Locate the public void Linq3() method in the code listing
+Locate the //TODO: comment and write LINQ code to implement the following functionality
+Write a LINQ statement to find all products that are in stock and cost more than 3.00 per unit.
+Run the method to verify the output.
+Locate the public void Linq4() method in the code listing
+Locate the //TODO: comment and write LINQ code to implement the following functionality
+Write a LINQ statement to find all customers in Washington (WA).  Review the data source and look under Region.
+Run the method to verify the output.
+For the second part of this assignment, complete the following:
+
+Open the LINQ Ordering Operators project file
+Locate the public void Linq30() method in the code listing
+Locate the //TODO: comment and write LINQ code to implement the following functionality
+Write a LINQ statement to sort a list of products by name in descending order as per the comments in the solution file.
+Run the method to verify the output.
+Locate the public void Linq32() method in the code listing
+Locate the //TODO: comment and write LINQ code to implement the following functionality
+Write a LINQ statement to use orderby and descending to sort a list of doubles from highest to lowest.
+Run the method to verify the output.
+Post your finished code in the Peer Review area.   You do NOT have to submit the entire .cs file, just the LINQ code that you have written.  If you want to verify a peer's code, paste their LINQ into your version of the project.
+*/
+
