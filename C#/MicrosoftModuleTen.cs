c665e91c248b094031abda411d6026462ce239a5
McKinnely <mckinnely.bentley@gmail.com>
2016-10-08 19:09:56 -0500

    Added Microsoft Module Ten Code Added

--- C#/MicrosoftModuleTen.cs
+++ C#/MicrosoftModuleTen.cs
@@ -24,3 +24,73 @@ Run the method to verify the output.
 Post your finished code in the Peer Review area.   You do NOT have to submit the entire .cs file, just the LINQ code that you have written.  If you want to verify a peer's code, paste their LINQ into your version of the project.
 */
 
+/* Reminder
+Post your finished code in the Peer Review area. You do NOT have to submit the entire .cs file, just the LINQ code that you have written. If you want to verify a peer's code, paste their LINQ into your version of the project.
+* /
+
+public void Linq3()
+{
+
+List<Product> products = GetProductList();
+var expensiveInStockProducts =
+from prod in products
+where prod.UnitPrice > 3
+select prod;
+
+Console.WriteLine("In-stock products that cost more than 3.00:");
+
+foreach (var product in expensiveInStockProducts)
+{
+Console.WriteLine("{0} is in stock and costs more than 3.00.", product.ProductName);
+}
+}
+
+public void Linq4()
+{
+List<Customer> customers = GetCustomerList();
+var waCustomers =
+from customersInWa in customers
+where customersInWa.Region == "WA"
+select customersInWa;
+//TODO: Create code to implement the functionality listed in the [Description] tag for this query
+
+Console.WriteLine("Customers from Washington and their orders:");
+foreach (var customer in waCustomers)
+{
+Console.WriteLine("Customer {0}: {1} {2}", customer.CustomerID, customer.CompanyName, customer.Region);
+foreach (var order in customer.Orders)
+{
+Console.WriteLine(" Order {0}: {1}", order.OrderID, order.OrderDate);
+
+}
+
+}
+
+}
+
+public void Linq30()
+{
+List<Product> products = GetProductList();
+
+var sortedProducts =
+from desName in products
+orderby desName.ProductName descending
+select desName;
+ObjectDumper.Write(sortedProducts);
+}
+
+public void Linq32()
+{
+double[] doubles = { 1.7, 2.3, 1.9, 4.1, 2.9 };
+var sortedDoubles =
+from desDoubles in doubles
+orderby desDoubles descending
+select desDoubles;
+//TODO: Enter code here to order this list in descending order
+
+Console.WriteLine("The doubles from highest to lowest:");
+foreach (var d in sortedDoubles)
+{
+Console.WriteLine(d);
+}
+}
