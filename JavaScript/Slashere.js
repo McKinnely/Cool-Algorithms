aae475a11a9c93edb07ff28fc77c039c4aaeb54f
McKinnely <mckinnely.bentley@gmail.com>
2016-10-10 23:20:59 -0500

    Finished FCC Slasher Algorithm Instructions.

--- JavaScript/Slashere.js
+++ JavaScript/Slashere.js
@@ -1,5 +1,8 @@
-
-
+/*
+Return the remaining elements of an array after chopping off n elements from the head.
+The head means the beginning of the array, or the zeroth index.
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+*/
 
 function slasher(arr, howMany) 
 {
