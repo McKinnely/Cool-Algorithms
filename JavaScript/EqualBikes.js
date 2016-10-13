5fcc9a8ee596024e938fd7ae41dfc54a7260b6d4
McKinnely <mckinnely.bentley@gmail.com>
2016-10-13 18:50:35 -0500

    Added Even Index Value Finder

---
+++ JavaScript/EqualBikes.js
@@ -0,0 +1,30 @@
+
+function findEvenIndex(arr)
+{
+  
+var size;
+size = arr.length;
+
+   if(size % 2 == 0)
+        {
+          size = size / 2;
+
+          if (arr[size -1] == arr[size])
+          {
+          	console.log( "Array values for the lasst value of middle of array and the first value of the middle of the array matches." );
+            console.log( "Values are: " + arr[size] + " and " + arr[size -1]);
+          }
+
+          console.log("No Match.")
+                 
+          //Even
+        }
+        else
+        {
+          console.log( size + " is odd.");
+
+          //Odd
+        }
+   }
+
+   findEvenIndex([1,2,3,5,5,9,7,8]);
