f3ba6ff2aa66ea226efe8757b2b61db104bce3b6
McKinnely <mckinnely.bentley@gmail.com>
2018-04-03 02:42:16 -0500

    Updated Palindrome Code

--- JavaScript/Palindrome.js
+++ JavaScript/Palindrome.js
@@ -11,66 +11,17 @@ We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and 
 */
 
 
-function reverseString(reversing)
-{
-          reversing = reversing.split("");
-          reversing = reversing.reverse();
-          reversing = reversing.join("");
-
-          return reversing;
-}
-
 function palindrome(str) 
 {
- 
-      //You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower 
-      //case in order to check for palindromes.
-
-      var removeCharacters = str.replace(/[^0-9a-zA-Z]/g, '');
-      removeCharacters = removeCharacters.toLowerCase();
-      
-      var  size = removeCharacters.length;
-      var  firstHalf;
-      var  secondHalf;
-
-        if(size == 1)
-          {
-            return; //Can't be.
-          }
-
-       
-       if(size % 2 == 0)
-        {
-          size = size / 2;
-          firstHalf  = removeCharacters.slice(0, -size);
-          secondHalf = removeCharacters.slice(size);
-          
-          secondHalf = reverseString(secondHalf);
-
-          //Even
-
-        }
-        else
-        {
-          //Odd
-          size--;
-          size = size / 2;
-          firstHalf  = removeCharacters.slice(0, size);
-
-          size++;
-          secondHalf = removeCharacters.slice(size);
-          secondHalf = reverseString(secondHalf);
-     
-        }
-      // console.log(removeCharacters);
-      if (firstHalf == secondHalf)
-      {
-      console.log("palindrome");
-       return true;
-      }
-       return false;
+  // Good luck!
+  str = str.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+  if(str === str.split('').reverse().join(''))
+  {
+   console.log("Palindrome!")
+   return true;
+  }
+  return false;
 }
-
 
 
 palindrome("E = MC^2, Force = Mass * Accelartion, fx(345n)");
---
+++ JavaScript/freeTester.js
@@ -0,0 +1,11 @@
+
+function palindrome(str) {
+  // Good luck!
+  var removeCharacters = str.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+  console.log(removeCharacters);
+  return true;
+}
+
+
+
+palindrome("EY99#e");
