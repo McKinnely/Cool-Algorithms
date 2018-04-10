2e16d16d981befb3c3e0ad53e4973cac66e765e4
McKinnely <mckinnely.bentley@gmail.com>
2018-04-10 02:25:47 -0500

    Updated LongestWord Algorithm Format

--- JavaScript/LongestWord.js
+++ JavaScript/LongestWord.js
@@ -6,22 +6,22 @@ Your response should be a number.
 
 
 function findLongestWord(str) 
-    {
-
-var longestWord = '';
-var words = str.split(" ");
-var counter = 0;
-
-for(var val of words)
 {
-    if(words[counter].length > longestWord.length)
+
+    var longestWord = '';
+    var words = str.split(" ");
+    var counter = 0;
+
+    for(var val of words)
     {
-       longestWord = words[counter];    
+        if(words[counter].length > longestWord.length)
+        {
+        longestWord = words[counter];    
+        }
+        counter++;
     }
-    counter++;
-}
-    console.log(longestWord);
-    console.log(longestWord.length);
-      
+        console.log(longestWord);
+        console.log(longestWord.length);
+        
  }
 findLongestWord("Energy can neither be created or destroyed!");
