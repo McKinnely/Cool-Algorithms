099f51071b8868f6df6bcbc5bced0fabaa63532a
McKinnely <mckinnely.bentley@gmail.com>
2018-04-10 02:19:09 -0500

    Updated LongestWord Algorithm

--- JavaScript/LongestWord.js
+++ JavaScript/LongestWord.js
@@ -3,19 +3,25 @@ Return the length of the longest word in the provided sentence.
 Your response should be a number.
 */
 
+
+
 function findLongestWord(str) 
     {
-    str = str.split(" ");
-    var size = str.length;
-    var counter = 0;
-    var arrayCopy = [];
-    while(size > counter )
-    { 
-    arrayCopy[counter] = str[counter].length;
-    counter++;
+
+var longestWord = '';
+var words = str.split(" ");
+var counter = 0;
+
+for(var val of words)
+{
+    if(words[counter].length > longestWord.length)
+    {
+       longestWord = words[counter];    
     }
-    var max = Math.max.apply(Math, arrayCopy);
-    var indexTopValueLocator = arrayCopy.indexOf(max);
-    return str[indexTopValueLocator].length;
-   }
+    counter++;
+}
+    console.log(longestWord);
+    console.log(longestWord.length);
+      
+ }
 findLongestWord("Energy can neither be created or destroyed!");
